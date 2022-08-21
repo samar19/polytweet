ff8bb45d9c137e43dd3ac4c27505e9fcaa8976f7
samar abdelhmeed <samar.abdelhmeed77@gmail.com>
2022-08-21 05:38:27 +0200

    pargma version error

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,23 +1,26 @@
-import { ethers } from "hardhat";
+const hre = require("hardhat");
+
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+ 
+  const ERC721 = await hre.ethers.getContractFactory("ERC721");
+  const poly = await ERC721.deploy(ERC721.address);
+  await poly.deployed();
+  console.log("ERC721 deployed to:", poly.address);
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  let config = `
+ 
+  export const ERC721address = "${poly.address}"
+  `
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  let data = JSON.stringify(config)
+  fs.writeFileSync('config.js', JSON.parse(data))
 
-  await lock.deployed();
-
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
